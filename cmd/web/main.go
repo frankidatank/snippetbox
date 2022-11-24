9429f400ae39e05d2c8622b3ba158a61076a0785
Frankie Murillo <frankidatank@gmail.com>
2022-11-24 13:35:16 -0800

    added go mysql driver as well as connection code in main

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 // Define an application struct to hold the application-wide dependencies for the
@@ -17,7 +20,7 @@ type application struct {
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
-
+	dsn := flag.String("dsn", "web:Frankiem1998!@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
@@ -28,6 +31,14 @@ func main() {
 		infoLog:  infoLog,
 	}
 
+	//connect to db
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
+
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
@@ -36,6 +47,18 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	//this is to test the actual connection to the database
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
